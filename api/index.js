2887781cbc2183a5ce863a9979825391fcbb9c34
Ashmit <lci2021013@gmail.com>
2024-05-02 14:39:28 +0530

    package json

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import regionRoutes from './routes/regions.route.js'
 import waterbodyRoutes from './routes/waterbodies.route.js'
 import sensorRoutes from './routes/sensors.route.js'
+import path from 'path'
 dotenv.config()
 const app = express()
 app.use(express.json())
@@ -15,12 +16,17 @@ mongoose.connect(process.env.MONGO).then(
 .catch((err)=>{
     console.log(err);
 })
+const __dirname = path.resolve()
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
 app.use('/api/regions',regionRoutes)
 app.use('/api/waterbodies',waterbodyRoutes)
 app.use('/api/sensors',sensorRoutes)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal Server Error'
